43c89d2d478c4f5bd2c725d136966ef86817b3c8
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2022-04-16 19:35:50 -0700

    .

---
+++ src/Component/Selector.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Button from './Button'
+
+const Selector = () => {
+    for( var i = 0; i < 10; i++){
+        <button>{i}</button>
+    }
+    
+  return (
+    <div>
+        <h1>React Selector</h1>
+    </div>
+  )
+}
+
+export default Selector
